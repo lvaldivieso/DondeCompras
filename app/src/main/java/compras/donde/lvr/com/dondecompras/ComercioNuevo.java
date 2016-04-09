67e0e1d3cef671dfb23a554877c4b1cb9c5bf26a
Leonardo Valdivieso <leonardo.valdivieso@gmail.com>
2016-04-09 18:32:53 -0300

    actividad nuevo comercio

--- app/src/main/java/compras/donde/lvr/com/dondecompras/ComercioNuevo.java
+++ app/src/main/java/compras/donde/lvr/com/dondecompras/ComercioNuevo.java
@@ -1,7 +1,23 @@
 package compras.donde.lvr.com.dondecompras;
 
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
+
 /**
  * Created by Leonardo on 09/04/2016.
  */
-public class ComercioNuevo {
+public class ComercioNuevo extends AppCompatActivity {
+
+    String idCategoria;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_nuevo_comercio);
+
+        idCategoria = (getIntent().getStringExtra("idCategoria"));
+        Toast.makeText(getApplicationContext(), idCategoria, Toast.LENGTH_SHORT).show();
+
+    }
 }
